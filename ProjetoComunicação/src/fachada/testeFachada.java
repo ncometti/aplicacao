fa38753ff6c8df9c28b7d5462140656d29527a98
ncometti <Natlia@Natlia-HP>
2013-03-18 23:47:02 -0300

    Refazendo o player e testando

---
+++ ProjetoComunicação/src/fachada/testeFachada.java
@@ -0,0 +1,18 @@
+package fachada;
+
+import java.io.File;
+
+import negocios.MP3Player;
+
+public class testeFachada {
+
+	
+	public static void main(String[] args) throws InterruptedException {
+		String path = "wonderwall.mp3";
+	    File mp3File = new File(path);
+	    MP3Player musica = new MP3Player(mp3File);
+	    if(MP3Player.isMusicaAtiva() == false) musica.start();	    
+
+	}
+
+}
