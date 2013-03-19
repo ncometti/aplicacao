9a5445f0b7d55f8dbebe7864938bf9ce4480c49d
Jonas Lins <jonaslins@hotmail.com>
2013-03-19 15:01:29 -0300

    Classes atualizadas: - Musica - gets and sets added
    Classes adicionadas :
      repositorios -> Repositorio - Interface
      repositorios -> RepositorioMusica - Classe
      negocios -> Gerenciador
      
    Pacotes adicionados:
     - repositorios

---
+++ ProjetoComunicação/src/repositorios/RepositorioMusica.java
@@ -0,0 +1,32 @@
+package repositorios;
+
+import java.io.File;
+
+import dados.Musica;
+
+public class RepositorioMusica implements Repositorio{
+
+	private File fileTmp;
+
+
+	public void inserir(Musica musica) {
+		// TODO 
+	}
+
+	public void remover(String musica) {
+		// TODO 
+	}
+
+	public boolean existe(String musica) {
+		// TODO 
+		return false;
+	}
+
+	public Object atualizarLista() {
+		// TODO 
+		return null;
+	}
+
+
+
+}
